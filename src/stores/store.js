55cb6571123c9f3d75dbc4149a7cc32cfbb3ac4e
Andrii Peliak <vseprav@gmail.com>
2017-03-29 16:44:20 +0300

    persist state to local store

--- src/stores/store.js
+++ src/stores/store.js
@@ -3,10 +3,18 @@ import { createLogger } from 'redux-logger'
 
 import fireBaseUser from '../reducers/reducers';
 
-export default createStore(
+let persistedState = localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState')) : {};
+
+let store = createStore(
   combineReducers({
     fireBaseUser
   }),
-  {},
+  persistedState,
   applyMiddleware(createLogger())
 );
+
+store.subscribe(()=>{
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+});
+
+export default store;
