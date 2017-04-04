650cbbeec867b5e6aecfb3377f4dfb7bd75f43c1
Andrii Peliak <vseprav@gmail.com>
2017-04-04 18:53:04 +0300

    update dictionaries list

--- src/components/dictionaries/components/DictionariesComponent.js
+++ src/components/dictionaries/components/DictionariesComponent.js
@@ -5,6 +5,17 @@ import React from 'react';
 require('styles/dictionaries/components/Dictionaries.css');
 
 class DictionariesComponent extends React.Component {
+
+  handleChange(e) {
+    let state = {};
+    state[`${e.target.name}`] = e.target.value;
+    this.setState(state);
+  }
+
+  saveDictionary() {
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div className="dictionaries-component">
@@ -17,7 +28,16 @@ class DictionariesComponent extends React.Component {
           <a href="#" className="list-group-item list-group-item-action">Porta ac consectetur ac</a>
           <a href="#" className="list-group-item list-group-item-action disabled">Vestibulum at eros</a>
         </div>
-        <button type="button" className="btn btn-primary">Add Dictionary</button>
+        <div className="form-group">
+          <div className="row">
+            <div className="col-6">
+              <input name="dictionary-name" type="text" onChange={this.handleChange.bind(this)} className="form-control" placeholder="Enter dictionary name"/>
+            </div>
+            <div className="col-4">
+              <button onClick={this.saveDictionary.bind(this)} type="button" className="btn btn-primary">Add Dictionary</button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
